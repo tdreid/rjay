6c228dab7fb9153c23847b70bd1b2f7ccb8b46be
tdreid <trevor.d.reid@gmail.com>
2018-10-30 00:34:27 +0000

    Change the title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Vika | Starter Application' });
+  res.render('index', { title: 'Rjay | A slim experiment' });
 });
 
 module.exports = router;
